fe93abe180d4222ea178b97c1040d40a407fe34c
Rishabh Surana <rishabhjainsurana@gmail.com>
2025-12-15 11:32:26 +0530

    CORSMiddleware update

--- Blog/main.py
+++ Blog/main.py
@@ -3,8 +3,22 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import blog, user, authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title = 'BlogStory')
+origins = [
+    "http://localhost:5173",  # React dev server
+    "https://your-frontend-domain.com",  # Production URL if deployed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # Allow these origins
+    allow_credentials=True,
+    allow_methods=["*"],          # Allow GET, POST, etc.
+    allow_headers=["*"],          # Allow headers like Authorization
+)
+
 
 
 # Home Page
